a97891d49008ef579f2d057e197fa3e86f89e0dc
LIERVILLE Fabien <flierville@cesi.fr>
2020-05-27 10:03:32 +0200

    Form

--- src/Controller/Admin/ProduitsController.php
+++ src/Controller/Admin/ProduitsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Produits;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitsController extends AbstractController
@@ -12,8 +14,34 @@ class ProduitsController extends AbstractController
      */
     public function index()
     {
-        return $this->render('admin/produits/index.html.twig', [
-            'controller_name' => 'ProduitsController',
+        $produitsRepository = $this->getDoctrine()->getRepository(Produits::class);
+        $produits = $produitsRepository->findAll();
+        return $this->render('admin/produits/index.html.twig',[
+            'produits' => $produits
         ]);
+
     }
+
+    /**
+     * @Route  ("/admin/produit/add", name="admin.produit.add")
+     */
+    public function add(){
+        return $this->render('admin/produits/add.html.twig');
+    }
+
+    /**
+     * @Route("/admin/produits/{id}", name="admin.produit.edit")
+     */
+    public function edit(){
+        return $this->render('admin/produits/edit.html.twig');
+    }
+
+
+    /**
+     * @Route("/admin/produit/delete/{id}", name="admin.produit.delete", methods="DELETE")
+     */
+    public function delete(Produits $produit,Request $request){
+        return $this->redirectToRoute('admin_produits');
+    }
+
 }
